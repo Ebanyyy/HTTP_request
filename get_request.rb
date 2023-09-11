58f0c97978965ea0eddfcb374601e61c08d9e4b3
Ebanyyy <vaniegabu@gmail.com>
2023-09-11 14:06:45 +0800

    2nd commit

--- get_request.rb
+++ get_request.rb
@@ -1,10 +1,14 @@
 #get request
 require 'net/http'
+require 'json'
 
-url = URI.parse('http://www.example.com/index.html')
-req = Net::HTTP::Get.new(url.to_s)
-res = Net::HTTP.start(url.host, url.port) {|http|
-	http.request(req)
-	}
+url = URI.parse('https://fwr.ventures/disclaimer.html')
+# req = Net::HTTP::Get.new(url.to_s)
+# res = Net::HTTP.start(url.host, url.port) {|http|
+# 	https.request(req)
+# 	}
+response = Net::HTTP.get_response(url)
+parsed_data = JSON.parse(response.body)
+puts parsed_data
 
-puts res.body
+
